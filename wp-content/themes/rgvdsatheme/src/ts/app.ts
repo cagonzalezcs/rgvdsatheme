f008462e2ce6ec1b958d88e2f472e90e7050900f
Cesar Gonzalez <cesar.a.gonzalezcs@gmail.com>
2026-07-01 19:54:15 -0500

    Introducing webawesome web component library

--- wp-content/themes/rgvdsatheme/src/ts/app.ts
+++ wp-content/themes/rgvdsatheme/src/ts/app.ts
@@ -1,1 +1,8 @@
+// Web Awesome Imports
+import "@awesome.me/webawesome/dist/styles/webawesome.css";
+import "@awesome.me/webawesome/dist/components/button/button.js";
+import "@awesome.me/webawesome/dist/components/input/input.js";
+import "@awesome.me/webawesome/dist/components/dropdown/dropdown.js";
+
+// Custom Styles
 import "../scss/app.scss";
